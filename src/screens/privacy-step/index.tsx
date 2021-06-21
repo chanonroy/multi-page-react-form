94df3b2a8ecf2db746e2fe3cadf0e57c5616525b
Chanon Roy <chanonroy@gmail.com>
2021-06-21 20:56:34 +0100

    feat: add checkbox component

--- src/screens/privacy-step/index.tsx
+++ src/screens/privacy-step/index.tsx
@@ -1,19 +1,45 @@
+import { useState } from 'react'
 import { PrivacyPayload } from '../../@types'
+import Button from '../../components/button'
 import Card from '../../components/card'
+import CheckboxInput from '../../components/checkbox-input'
 import Container from '../../components/container'
-import TextInput from '../../components/text-input'
-import TextInputLabel from '../../components/text-input-label'
 
 export default function PrivacyForm({
   onSubmit,
 }: {
   onSubmit: (payload: PrivacyPayload) => void
 }) {
+  const [receiveUpdates, setReceiveUpdates] = useState(false)
+  const [receiveCommunication, setReceiveCommunication] = useState(false)
+
+  const handleSubmit = () => {
+    onSubmit({
+      receiveUpdates,
+      receiveCommunication,
+    })
+  }
+
   return (
     <Container style={{ paddingTop: 40, paddingBottom: 20 }}>
       <Card>
-        <TextInputLabel>Name</TextInputLabel>
-        <TextInput placeholder='Jane Smith' />
+        <div style={{ marginBottom: 20 }}>
+          <CheckboxInput
+            label='Receive updates about tray.io product by email'
+            checked={receiveUpdates}
+            onChange={setReceiveUpdates}
+          />
+        </div>
+        <div style={{ marginBottom: 40 }}>
+          <CheckboxInput
+            label='Receive communication by email for other products created by the Tray.io team'
+            checked={receiveCommunication}
+            onChange={setReceiveCommunication}
+          />
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Button onClick={handleSubmit}>Submit</Button>
+        </div>
       </Card>
     </Container>
   )
